336d8fbdc4897f15beae91f74d707b792ced72bf
Vu Minh Quan <quan2082003@gmail.com>
2025-07-30 13:52:17 +0700

    fix build

--- src/store/sessionStore.ts
+++ src/store/sessionStore.ts
@@ -1,12 +1,12 @@
-import { create } from "zustand";
-import { persist } from "zustand/middleware";
+// import { create } from "zustand";
+// import { persist } from "zustand/middleware";
 
-export type SessionState = {};
+// export type SessionState = {};
 
-export type SessionActions = {};
+// export type SessionActions = {};
 
-export const useSessionStore = create<SessionState & SessionActions>()(
-  persist((set) => ({}), {
-    name: "session-store",
-  })
-);
+// export const useSessionStore = create<SessionState & SessionActions>()(
+//   persist((set) => ({}), {
+//     name: "session-store",
+//   })
+// );
